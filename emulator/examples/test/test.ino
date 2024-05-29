c6c30ab1b399ae672ee422b43a339ca985579a96
dennisppaul <dennis.paul@hfk-bremen.de>
2024-05-29 18:20:34 +0200

    added example

---
+++ emulator/examples/test/test.ino
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <iostream>
+
+void setup() {
+//     printf("setup()\n\r");
+    std::cout << "setup()" << std::endl;
+}
+
+void loop() {
+//     printf("loop()\n\r");
+    std::cout << "loop()" << std::endl << std::flush;
+}
