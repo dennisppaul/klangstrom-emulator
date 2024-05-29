693d0008f2d0b1275e2e39eda28a1d3a37d89da6
dennisppaul <dennis.paul@hfk-bremen.de>
2024-05-29 21:41:27 +0200

    added display font

--- emulator/cores/sdl2/main.cpp
+++ emulator/cores/sdl2/main.cpp
@@ -37,9 +37,22 @@ void sketch_loop() {
 
 class UmgebungApp : public PApplet {
 
-    PVector mVector{16, 16};
-    PShape  mShape;
-    int     mouseMoveCounter = 0;
+    PVector           mVector{16, 16};
+    PShape            mShape;
+    int               mouseMoveCounter = 0;
+    std::string       mFontPath        = sketchPath();
+    const std::string mFontName        = "JetBrainsMonoNL-Regular.ttf";
+    PFont*            mFont;
+
+    void arguments(std::vector<std::string> args) {
+        for (auto& s: args) {
+            println("> ", s);
+            if (begins_with(s, "--fontpath=")) {
+                println("found fontpath: ", get_string_from_argument(s));
+                mFontPath = get_string_from_argument(s);
+            }
+        }
+    }
 
     void settings() {
         size(1024, 768);
@@ -51,14 +64,26 @@ class UmgebungApp : public PApplet {
     }
 
     void setup() {
-        println("width : ", width);
-        println("height: ", height);
+        println("sketchpath: ", sketchPath());
+        println("fontpath  : ", mFontPath);
+        println("width     : ", width);
+        println("height    : ", height);
+
+        if (exists(mFontPath + "/" + mFontName)) {
+            mFont = loadFont(mFontPath + "/" + mFontName, 32);
+            textFont(mFont);
+        } else {
+            println("could not load font: ", (mFontPath + "/" + mFontName));
+        }
+
         sketch_setup();
     }
 
     void draw() {
-        sketch_loop();
         background(1);
+
+        fill(0);
+        text("23", 10, height - 20);
 
         stroke(0);
         noFill();
@@ -67,6 +92,8 @@ class UmgebungApp : public PApplet {
         noStroke();
         fill(random(0, 0.2));
         rect(20, 20, width / 2 - 40, height / 2 - 40);
+
+        sketch_loop();
     }
 
     void audioblock(float** input, float** output, int length) {
@@ -82,7 +109,6 @@ class UmgebungApp : public PApplet {
         if (key == 'q') {
             exit();
         }
-        println((char) key, " pressed");
     }
 };
 
